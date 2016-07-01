367d24603631084265dda6b1da188a0a42eba5c2
U-RFG\p000521338 <p000521338@A050836023.rfg.rfgad.ricoh.com>
2016-07-01 13:22:41 +0900

    modify

--- ch1_practice/practice1.2/practice1.2_test.go
+++ ch1_practice/practice1.2/practice1.2_test.go
@@ -5,17 +5,32 @@ import (
 	"testing"
 )
 
-func TestEcho(t *testing.T) {
+// 数値のテスト.
+func TestNumEcho(t *testing.T) {
 
 	testfile := []string{"1", "9", "8", "100", "11"}
-	want := "0 1\n1 9\n2 8\n3 100\n4 11 \n"
+	want := "0 1\n1 9\n2 8\n3 100\n4 11\n"
 	out = new(bytes.Buffer)
 	result := out.(*bytes.Buffer)
 
 	echo(testfile)
 
 	if result.String() != want {
-		t.Errorf("input: 10 100 100\\n, Output: %s", result.String())
+		t.Errorf("NumTestError Output: %s", result.String())
 	}
+}
 
+// 文字列のテスト.
+func TestStrEcho(t *testing.T) {
+
+	testfile := []string{"apple", "orange", "pine", "banana", "lemon"}
+	want := "0 apple\n1 orange\n2 pine\n3 banana\n4 lemon\n"
+	out = new(bytes.Buffer)
+	result := out.(*bytes.Buffer)
+
+	echo(testfile)
+
+	if result.String() != want {
+		t.Errorf("StrTestError Output: %s", result.String())
+	}
 }
