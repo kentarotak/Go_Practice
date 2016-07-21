7ee5204fa459f9429e5e0877be5d82b1547788ea
kentaro <kentaro.tak@gmail.com>
2016-07-22 07:47:30 +0900

    modify

--- ch1_practice/practice1.4/practice1.4_test.go
+++ ch1_practice/practice1.4/practice1.4_test.go
@@ -1,34 +1,33 @@
 package main
 
-
 import (
 	"fmt"
 	"os"
-  "testing"
+	"testing"
 )
 
 // 空のtextが指定されたときに、
 // mapの要素に何も入らずに終了すること
 func TestStringEmpty(t *testing.T) {
 
-  counts := make(map[string]int)
+	counts := make(map[string]int)
 	filenames := make(map[string]string)
 	f, err := os.Open("empty.txt")
 
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "text not found", err)
-  }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "text not found", err)
+	}
 
-  filecountLines(f, counts, "empty.txt", filenames)
+	filecountLines(f, counts, "empty.txt", filenames)
 
-  if len(counts) != 0 {
-    t.Errorf("TestStringEmpty error ")
-  }
+	if len(counts) != 0 {
+		t.Errorf("TestStringEmpty error ")
+	}
 
 }
 
 type testdata struct {
-	key string
+	key   string
 	count int
 }
 
@@ -36,31 +35,36 @@ type testdata struct {
 // 第二引数に指定されたtextの要素が仕様に沿って出てくること
 func TestStringEmptyAndElement(t *testing.T) {
 
-	var tests = []struct{
-			testval string
-			expect int
-		}{
-			{"apple", 2},
-			{"orange", 1},
-		}
+	var tests = []struct {
+		testval         string
+		expect          int
+		expect_filename string
+	}{
+		{"apple", 2, "\ttestfile.txt"},
+		{"orange", 1, "\ttestfile.txt"},
+	}
 
-  counts := make(map[string]int)
+	counts := make(map[string]int)
 	filenames := make(map[string]string)
 	files := []string{"empty.txt", "testfile.txt"}
 
 	for _, arg := range files {
 		f, err := os.Open(arg)
-	  if err != nil {
-	    fmt.Fprintf(os.Stderr, "text not found", err)
-	  }
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "text not found", err)
+		}
 
-	  filecountLines(f, counts, arg, filenames)
+		filecountLines(f, counts, arg, filenames)
 	}
 
 	for _, test := range tests {
 		if got := counts[test.testval]; got != test.expect {
 			t.Errorf("err %s, %v = %v", test.testval, test.expect, got)
 		}
+		if got := filenames[test.testval]; got != test.expect_filename {
+			t.Errorf("file err %s, %v = %v", test.testval, test.expect_filename, got)
+		}
+
 	}
 
 }
@@ -69,31 +73,35 @@ func TestStringEmptyAndElement(t *testing.T) {
 // 仕様に従って,mapファイルにデータが入力されること
 func TestStringNormalTest(t *testing.T) {
 
-	var tests = []struct{
-			testval string
-			expect int
-		}{
-			{"apple", 3},
-			{"orange", 3},
-			{"pine", 4},
-		}
+	var tests = []struct {
+		testval         string
+		expect          int
+		expect_filename string
+	}{
+		{"apple", 3, "\ttestfile.txt\ttestfile2.txt"},
+		{"orange", 3, "\ttestfile.txt\ttestfile2.txt"},
+		{"pine", 4, "\ttestfile2.txt"},
+	}
 
-  counts := make(map[string]int)
+	counts := make(map[string]int)
 	filenames := make(map[string]string)
 	files := []string{"testfile.txt", "testfile2.txt"}
 
 	for _, arg := range files {
 		f, err := os.Open(arg)
-	  if err != nil {
-	    fmt.Fprintf(os.Stderr, "text not found", err)
-	  }
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "text not found", err)
+		}
 
-	  filecountLines(f, counts, arg, filenames)
+		filecountLines(f, counts, arg, filenames)
 	}
 
 	for _, test := range tests {
 		if got := counts[test.testval]; got != test.expect {
 			t.Errorf("err %s, %v = %v", test.testval, test.expect, got)
+		}
+		if got := filenames[test.testval]; got != test.expect_filename {
+			t.Errorf("file err %s, %v = %v", test.testval, test.expect_filename, got)
 		}
 	}
 
@@ -103,31 +111,35 @@ func TestStringNormalTest(t *testing.T) {
 // 仕様に従って,mapファイルにデータが入力されること
 func TestStringNormalTest2(t *testing.T) {
 
-	var tests = []struct{
-			testval string
-			expect int
-		}{
-			{"apple", 3},
-			{"orange", 3},
-			{"pine", 4},
-		}
+	var tests = []struct {
+		testval         string
+		expect          int
+		expect_filename string
+	}{
+		{"apple", 3, "\ttestfile2.txt\ttestfile.txt"},
+		{"orange", 3, "\ttestfile2.txt\ttestfile.txt"},
+		{"pine", 4, "\ttestfile2.txt"},
+	}
 
-  counts := make(map[string]int)
+	counts := make(map[string]int)
 	filenames := make(map[string]string)
 	files := []string{"testfile2.txt", "testfile.txt"}
 
 	for _, arg := range files {
 		f, err := os.Open(arg)
-	  if err != nil {
-	    fmt.Fprintf(os.Stderr, "text not found", err)
-	  }
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "text not found", err)
+		}
 
-	  filecountLines(f, counts, arg, filenames)
+		filecountLines(f, counts, arg, filenames)
 	}
 
 	for _, test := range tests {
 		if got := counts[test.testval]; got != test.expect {
 			t.Errorf("err %s, %v = %v", test.testval, test.expect, got)
+		}
+		if got := filenames[test.testval]; got != test.expect_filename {
+			t.Errorf("file err %s, %v = %v", test.testval, test.expect_filename, got)
 		}
 	}
 
