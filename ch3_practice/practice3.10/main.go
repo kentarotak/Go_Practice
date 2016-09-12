01dd6cd937a2ca775e18aa1363b52dd13df175ab
kentaro <kentaro.tak@gmail.com>
2016-09-12 12:57:34 +0900

    up

---
+++ ch3_practice/practice3.10/main.go
@@ -0,0 +1,59 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+	}
+}
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s) - 1
+	if n < 3 {
+		return s
+	}
+
+	var buf bytes.Buffer
+
+	for i := 0; i <= n; i++ {
+		if i%3 == 0 && i != 0 {
+			buf.WriteString(",")
+		}
+		fmt.Fprintf(&buf, "%c", s[n-i])
+	}
+
+	return Reverse(buf.String())
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+//!-
