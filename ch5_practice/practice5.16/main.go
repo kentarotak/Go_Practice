9dd5768826128aeb0940c31d384f2bda0baca9e4
kentaro <kentaro.tak@gmail.com>
2016-10-16 18:32:06 +0900

    up

---
+++ ch5_practice/practice5.16/main.go
@@ -0,0 +1,37 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 142.
+
+// The sum program demonstrates a variadic function.
+package main
+
+import "fmt"
+
+func Join(sep string, a ...string) string {
+	if len(a) == 0 {
+		return ""
+	}
+	if len(a) == 1 {
+		return a[0]
+	}
+	n := len(sep) * (len(a) - 1)
+	for i := 0; i < len(a); i++ {
+		n += len(a[i])
+	}
+
+	b := make([]byte, n)
+	bp := copy(b, a[0])
+	for _, s := range a[1:] {
+		bp += copy(b[bp:], sep)
+		bp += copy(b[bp:], s)
+	}
+	return string(b)
+}
+
+//!-
+
+func main() {
+	//!+main
+	fmt.Printf("%s\n", Join(","))
+}
