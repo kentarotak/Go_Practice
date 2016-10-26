7119b85797648cf8362f0e8dbc5a6533a85216e1
kentaro <kentaro.tak@gmail.com>
2016-10-26 20:57:05 +0900

    up

---
+++ ch5_practice/practice5.14/main.go
@@ -0,0 +1,74 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 139.
+
+// Findlinks3 crawls the web, starting with the URLs on the command line.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopl.io/ch5/links"
+)
+
+//!+breadthFirst
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+//!-breadthFirst
+
+//!+crawl
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	for _, link := range list {
+		fmt.Printf("%s\n", link)
+	}
+	return list
+}
+
+func searchDir(path string) []string {
+
+	var list []string
+	files, _ := ioutil.ReadDir(path)
+	for _, f := range files {
+		fmt.Println(f.Name())
+		if f.IsDir() {
+			list = append(list, path+f.Name())
+		}
+	}
+
+	return list
+}
+
+//!-crawl
+
+//!+main
+func main() {
+	// Crawl the web breadth-first,
+	// starting from the command-line arguments.
+	breadthFirst(searchDir, os.Args[1:])
+}
+
+//!-main
