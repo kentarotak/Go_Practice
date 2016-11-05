8e9814c902afeba6f3c57d2750d02f2e9e1c21e9
kentaro <kentaro.tak@gmail.com>
2016-11-05 17:27:24 +0900

    up

---
+++ ch7_practice/Ex7.03/sort_test.go
@@ -0,0 +1,20 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package treesort
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	data := make([]int, 50)
+	for i := range data {
+		data[i] = rand.Int() % 50
+	}
+	Sort(data)
+
+	root.String()
+
+}
