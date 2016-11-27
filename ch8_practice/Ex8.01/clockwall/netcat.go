2b4bd435eecbcd5a5e6db8e91e669fc5de9a4730
kentaro <kentaro.tak@gmail.com>
2016-11-27 08:47:07 +0000

    up

---
+++ ch8_practice/Ex8.01/clockwall/netcat.go
@@ -0,0 +1,39 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 221.
+//!+
+
+// Netcat1 is a read-only TCP client.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	data := new(bytes.Buffer)
+	if _, err := io.Copy(data, src); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(dst, "\r%s", data)
+
+}
+
+//!-
