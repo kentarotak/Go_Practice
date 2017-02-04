157fae2355327ffdecc99449edf2e6cd6210966a
kentaro <kentaro.tak@gmail.com>
2017-02-04 20:47:09 +0900

    up

---
+++ ch12_practice/Ex12.3/sexpr_test.go
@@ -0,0 +1,103 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package sexpr
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Example_test() {
+	var b bool
+
+	b = true
+
+	data, err := Marshal(b)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("Marshal() = %s\n", data)
+
+	data, err = Marshal(10)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	data, err = Marshal(10.6)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	cmp := complex(3, 5)
+	data, err = Marshal(cmp)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	// Output:
+	// Marshal() = t
+	// Marshal() = 10
+	// Marshal() = 10.600000
+	// Marshal() = #C(3.000000 5.000000)
+}
+
+// Test verifies that encoding and decoding a complex data value
+// produces an equal result.
+//
+// The test does not make direct assertions about the encoded output
+// because the output depends on map iteration order, which is
+// nondeterministic.  The output of the t.Log statements can be
+// inspected by running the test with the -v flag:
+//
+// 	$ go test -v gopl.io/ch12/sexpr
+//
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var movie Movie
+	if err := Unmarshal(data, &movie); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", movie)
+
+	// Check equality.
+	if !reflect.DeepEqual(movie, strangelove) {
+		t.Fatal("not equal")
+	}
+
+	// Pretty-print it:
+	data, err = MarshalIndent(strangelove)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("MarshalIdent() = %s\n", data)
+}
