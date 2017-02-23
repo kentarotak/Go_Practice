8c7446224e6714b9eb2047687de1952bfa58ba53
kentaro <kentaro.tak@gmail.com>
2017-02-23 19:38:42 +0900

    up

---
+++ ch13_practice/Ex13.1_unsolve/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+
+func deepEqualNumeric(x, y interface{}) {
+
+	var epsilon float64
+	epsilon = 0.00000000010
+
+}
+
+func convertFloat64(v interface{}) (float64 error) {
+
+	switch v := v.(type) {
+	case int, int16, int32, int64:
+
+	}
+
+}
